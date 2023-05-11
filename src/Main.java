5664174009cd728976d8f0adaad6066caf000b20
valen <valentin.dumler@gmail.com>
2023-05-11 15:24:33 +0200

    feat class Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    House houseFirst = new PrivateHouse("Berliner Str 5");
+    House houseSecond = new PrivateHouse("Potsdam Str 25");
+    House houseThird = new ApartmentsHouse("Wilhelm Str 55", 5);
+
+    List<House> ListOfHouses = new ArrayList<>();
+    ListOfHouses.add(houseFirst);
+    ListOfHouses.add(houseSecond);
+    ListOfHouses.add(houseThird);
+
+    for (House house : ListOfHouses) {
+      System.out.println(house);
+    }
   }
 }
