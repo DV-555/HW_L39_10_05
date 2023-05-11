509ef3a060f0962a9d3eefce33dc06490120e187
valen <valentin.dumler@gmail.com>
2023-05-11 14:41:50 +0200

    feat class ApartmentsHouse

---
+++ src/ApartmentsHouse.java
@@ -0,0 +1,19 @@
+public class ApartmentsHouse extends House {
+  private int numberOfEntrances;
+  public ApartmentsHouse(String address, int numberOfEntrances) {
+    super(address);
+    this.numberOfEntrances = numberOfEntrances;
+  }
+  public int getNumberOfEntrances() {
+    return numberOfEntrances;
+  }
+  public void setNumberOfEntrances(int numberOfEntrances) {
+    this.numberOfEntrances = numberOfEntrances;
+  }
+  @Override
+  public String toString() {
+    return "ApartmentsHouse{" +
+        "numberOfEntrances=" + numberOfEntrances +
+        '}';
+  }
+}
